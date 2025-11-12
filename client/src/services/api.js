e67d9da909730129be7be7dd8e62ff2b19dc62dc
Jeff Sokol <JeffSokolDev@gmail.com>
2025-11-12 16:52:41 -0500

    proper api calls

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,14 +2,14 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5050/api'
 
 //GET ALL PRODUCTS
 export const getProducts = async () =>{
-    const response = await fetch(`${API_BASE_URL}/products`)
+    const response = await fetch(`${API_BASE_URL}/api/products`)
     if(!response.ok) throw new Error('Failed to fetch products')
         return response.json()
 }
 
 //GET SINGLE PRODUCT BY ID
 export const getProduct = async (id) =>{
-    const response = await fetch(`${API_BASE_URL}/products/${id}`)
+    const response = await fetch(`${API_BASE_URL}/api/products/${id}`)
     if(!response.ok) throw new Error('Failed to fetch product')
         return response.json()
 }
@@ -23,14 +23,14 @@ export const getBoxSizes = async () =>{
 
 //GET BAG SIZES
 export const getBagSizes = async () =>{
-    const response = await fetch(`${API_BASE_URL}/products/sizes/bags`)
+    const response = await fetch(`${API_BASE_URL}api/products/sizes/bags`)
     if(!response.ok) throw new Error(`Failed to fetch bag sizes`)
         return response.json()
 }
 
 //GET GRIND TYPES
 export const getGrindTypes = async () =>{
-    const response = await fetch(`${API_BASE_URL}/products/grind-types`)
+    const response = await fetch(`${API_BASE_URL}api/products/grind-types`)
     if(!response.ok) throw new Error('Failed to fetch grind types')
         return response.json()
 }
